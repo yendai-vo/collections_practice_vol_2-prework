1a652f772d9a55b1b7ba384c13e3e8f24a87dc89
Cindy Vo <yendai.vo@hotmail.com>
2018-07-15 00:07:44 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,3 +37,25 @@ def remove_non_strings(arr)
   }
   result
 end
+
+
+def count_elements(arr)
+  result = []
+
+  arr.each {|item|
+    exists = false
+    result.each {|result|
+      if result[:name] == item[:name]
+        result[:count] += 1
+        exists = true
+      end
+    }
+    if(exists == false)
+      temp_item = item
+      temp_item[:count] = 1
+      result.push(temp_item)
+    end
+  }
+  
+  result
+end
